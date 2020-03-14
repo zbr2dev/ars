70ad8aafda6908db3e8bcd1cbc4e95fd4efb7f8a
zbr2dev <andrew@MacBook-Pro-Andrew.local>
2020-03-14 18:45:36 +0200

    corrected styles of animation, added rocket animation

--- scripts/main.js
+++ scripts/main.js
@@ -12,12 +12,16 @@
         $('.rocket-section .btn-plus-circle').on('click',function(){
             //console.log(calcPositionBetweenElem($(this),$currentContainer));
             $currentContainer = $(this).parent();
+            $image = $('img',$currentContainer);
             $desc = $('.desc',$currentContainer);
             $timeAnimation = 1000; //1sec
             if(!$currentContainer.hasClass('opened')){
+                var initialContainerHeight = $currentContainer.height();
                 var startPositionBtn = calcPositionBetweenElem($currentContainer,$(this));
                 window.localStorage.setItem("top", startPositionBtn.top);
                 window.localStorage.setItem("left", startPositionBtn.left);
+                window.localStorage.setItem("containerWidth", initialContainerHeight);
+                window.localStorage.setItem("leftPosImage", $image.css("left"));
             }
             $currentContainer.toggleClass('opened');
             if(!$currentContainer.hasClass('opened'))
@@ -25,12 +29,18 @@
                 $desc.removeClass('animated').addClass('fadeOutDown');
                 $currentContainer.animate({
                     "max-width" : "270px",
+                    "min-height": window.localStorage.getItem('containerWidth')
                 },$timeAnimation - 300);
+                $('img',$currentContainer).animate({
+                    // top: $image.css('top'),
+                    left: window.localStorage.getItem('leftPosImage')
+                },$timeAnimation);
                 $(this).css('position','absolute');
                 $(this).animate({
                     'top' : window.localStorage.getItem("top"),
                     'left' : window.localStorage.getItem("left"),
-                    'right' : "unset"
+                    'right' : "unset",
+                    //"margin-left": 0,
                 },$timeAnimation);
                 $desc.addClass('animated');
                 //setTimeout($desc.hide(),$timeAnimation + 100);
@@ -38,10 +48,16 @@
                 $desc.show().toggleClass('animated');
                 $currentContainer.animate({
                     "max-width" : "470px",
+                    "min-height" : "250px"
                 },$timeAnimation - 300);
+                $image.animate({
+                   // "top" : $image.css("top"),
+                    "left" : "30px"
+                })
+                
                 $(this).animate({
                     'top' : "15px",
-                    'right' : "15px"
+                    'right' : "15px",
                 },$timeAnimation);
             }
         });
